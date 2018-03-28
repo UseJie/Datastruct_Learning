f14c94f34c900b1112c952249690fd3e5e849683
guijiezheng1119@gmail.com <zhengguijie1119@outlook.com>
2018-03-28 10:59:15 +0800

    update

---
+++ StackList/StackList.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ElementType int
+#define false 0
+#define ERROR 0
+#define true 1
+#define bool int
+
+
+typedef struct SNode *PtrToSNode;
+struct SNode {
+	ElementType Data;
+	PtrToSNode Next;
+};
+typedef PtrToSNode Stack;
+
+Stack CreateStack()
+{/*创建堆栈*/
+	Stack S;
+	
+	S = (Stack)malloc(sizeof(struct SNode));
+	S->Next = NULL;
+	return S;
+}
+
+bool IsEmpty( Stack S )
+{
+	return( S->Next == NULL);
+}
+
+bool Push ( Stack S, ElementType X )
+{/*进栈 X为入栈数据*/
+	PtrToSNode TmpCell;
+	
+	TmpCell = (PtrToSNode)malloc(sizeof(struct SNode ));
+	TmpCell->Data = X;
+	TmpCell->Next = S->Next;
+	S->Next = TmpCell;
+	return true;
+}
+
+ElementType Pop( Stack S )
+{
+	PtrToSNode FirstCell;
+	ElementType TopElem;
+	
+	if( IsEmpty(S) ) {
+		printf( "堆栈空" );
+		return ERROR;
+	}
+	else {
+		FirstCell = S->Next;
+		TopElem = FirstCell->Data;
+		S->Next = FirstCell->Next;
+		free(FirstCell);
+		return TopElem;
+	}
+}
+
+int main()
+{
+	PtrToSNode S;
+	S = (PtrToSNode)malloc(sizeof(struct SNode));
+	S = CreateStack();
+	Push( S, 3);
+	printf("%d", Pop(S));
+	return 0;
+}
